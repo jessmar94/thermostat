4f7af9cb2aeebe56511f28758f56333030bd5a8c
Mezela <p_mezue@hotmail.com>
2019-10-16 15:07:05 +0100

    Add functionality to increase and decrease temp with buttons

--- scripts.js
+++ scripts.js
@@ -3,4 +3,15 @@ $(document).ready(function() {
   var thermostat = new Thermostat()
   $("#temperature").text(thermostat.temp) 
 
+  $("#temperature-up").click(function() {
+    thermostat.up();
+    $("#temperature").text(thermostat.temp)
+  })
+
+  $("#temperature-down").click(function() {
+    thermostat.down();
+    $("#temperature").text(thermostat.temp)
+  })
+
+
 });
